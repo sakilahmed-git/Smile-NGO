84742688ea2e59c330130463ade1937408ee431b
Shakeel Ahmed <sakilahme.user@gmail.com>
2026-08-06 00:35:03 +0530

    numb

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -3,10 +3,10 @@ export const siteConfig = {
   description:
     "A transparent, community-led NGO platform supporting education, health, nutrition and dignity programs for children and families.",
   url: "https://smilengo.org",
-  phone: "+91 98765 43210",
+  phone: "+91 7002372041/+91 7002683620",
   email: "hello@smilengo.org",
   address: "SMILE Community Centre, Patna, Bihar, India",
-  whatsapp: "919876543210",
+  whatsapp: "917002372041",
   social: {
     facebookPageUrl:
       process.env.NEXT_PUBLIC_FACEBOOK_PAGE_URL ?? "https://www.facebook.com/profile.php?id=61587482858117",
