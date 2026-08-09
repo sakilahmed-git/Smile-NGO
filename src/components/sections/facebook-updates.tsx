116e059aeee4285db2a76ee7d63ea88a86dced32
Shakeel Ahmed <sakilahme.user@gmail.com>
2026-08-09 17:03:14 +0530

    social2

--- src/components/sections/facebook-updates.tsx
+++ src/components/sections/facebook-updates.tsx
@@ -5,7 +5,7 @@ export function FacebookUpdates() {
         src="https://widgets.sociablekit.com/facebook-page-posts/iframe/25703916"
         frameBorder="0"
         width="100%"
-        height="2000"
+        height="1000"
         title="SMILE NGO Facebook Page Posts"
         loading="lazy"
         scrolling="yes"
